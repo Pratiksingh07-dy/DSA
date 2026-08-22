bd3b77d38117e8d5a34d6bcbb24bf199476454d4
Pratiksingh07-dy <pratiksingh2205@gmail.com>
2026-08-22 11:38:19 +0530

    Create 3622_Check_Divisibility_by_Digit_Sum_and_Product.py

---
+++ Math/3622_Check_Divisibility_by_Digit_Sum_and_Product.py
@@ -0,0 +1,21 @@
+def checkDivisibility(n):
+    temp = n
+    digit_sum = 0
+    digit_product = 1
+
+    while temp > 0:
+        digit = temp % 10
+
+        digit_sum += digit
+        digit_product *= digit
+
+        temp //= 10
+
+    total = digit_sum + digit_product
+
+    return n % total == 0
+
+
+n = 99
+
+print(checkDivisibility(n))
