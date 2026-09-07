8b825643763f2bb1c048100f376ae64601912d40
Pratiksingh07-dy <pratiksingh2205@gmail.com>
2026-09-07 23:54:07 +0530

    Update string_to_integer_atoi.py

---
+++ Arrays/string_to_integer_atoi.py
@@ -0,0 +1,43 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+
+        i = 0
+        n = len(s)
+
+        while i < n and s[i] == ' ':
+            i += 1
+
+        sign = 1
+
+        if i < n and s[i] == '-':
+            sign = -1
+            i += 1
+        elif i < n and s[i] == '+':
+            i += 1
+
+        num = 0
+
+        while i < n and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        num *= sign
+
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        if num < INT_MIN:
+            return INT_MIN
+
+        if num > INT_MAX:
+            return INT_MAX
+
+        return num
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    s = " -042"
+
+    print(solution.myAtoi(s))
