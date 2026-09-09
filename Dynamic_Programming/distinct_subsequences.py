95ac61217e3b20dd1a81709b119db8cef95a5c6c
Pratiksingh07-dy <pratiksingh2205@gmail.com>
2026-09-09 23:17:49 +0530

    Update distinct_subsequences.py

---
+++ Dynamic_Programming/distinct_subsequences.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+
+        m = len(s)
+        n = len(t)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m + 1):
+            dp[i][0] = 1
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+
+        return dp[m][n]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    s = "rabbbit"
+    t = "rabbit"
+
+    print(solution.numDistinct(s, t))
