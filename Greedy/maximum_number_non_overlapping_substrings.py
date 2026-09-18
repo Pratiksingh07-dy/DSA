ec46a1f92cf974100f8bf4dc36e616c4cb64ac58
Pratiksingh07-dy <pratiksingh2205@gmail.com>
2026-09-18 17:27:34 +0530

    Create maximum_number_non_overlapping_substrings.py

---
+++ Greedy/maximum_number_non_overlapping_substrings.py
@@ -0,0 +1,56 @@
+class Solution:
+    def maxNumOfSubstrings(self, s: str) -> list[str]:
+        n = len(s)
+
+        first = [n] * 26
+        last = [-1] * 26
+
+        for i, ch in enumerate(s):
+            x = ord(ch) - ord('a')
+            first[x] = min(first[x], i)
+            last[x] = i
+
+        intervals = []
+
+        for c in range(26):
+            if first[c] == n:
+                continue
+
+            left = first[c]
+            right = last[c]
+
+            i = left
+            valid = True
+
+            while i <= right:
+                x = ord(s[i]) - ord('a')
+
+                if first[x] < left:
+                    valid = False
+                    break
+
+                right = max(right, last[x])
+                i += 1
+
+            if valid:
+                intervals.append((left, right))
+
+        intervals.sort(key=lambda x: x[1])
+
+        result = []
+        prev_end = -1
+
+        for left, right in intervals:
+            if left > prev_end:
+                result.append(s[left:right + 1])
+                prev_end = right
+
+        return result
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    s = "adefaddaccc"
+
+    print(solution.maxNumOfSubstrings(s))
